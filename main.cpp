977c95034c1bb5d2c7893899d8ce4b9b13852e3c
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2024-09-09 23:24:31 +0200

    C++

--- main.cpp
+++ main.cpp
@@ -12,13 +12,32 @@ int main () {
 
 	double * average = new double [2];
 	long * count = new long [2];
+	bool * started = new bool [2];
 
 	file.open("samples/95MHz-center-100MHzCW", ios::in | ios::binary);
 
 	do {
 		file.read((char*) buffer, buffer_length);
 		cout << "read " << file.gcount() << "\n";
+
+		for(int i = 0; i < file.gcount(); i++) {
+			int type = i % 2;
+
+			if(started[type] == false) {
+				count[type] = 1;
+				average[type] = buffer[i];
+
+				started[type] = true;
+			} else {
+				average[type] = ((double) count[type] / (double)(count[type] + 1) * (double)average[type]) + ((double)buffer[i] / (count[type] + 1));
+
+				count[type]++;
+			}
+		}
 	} while(file);
+
+	cout << "I " << average[0] << "\n";
+	cout << "Q " << average[1] << "\n";
 
 	return 0;
 }
