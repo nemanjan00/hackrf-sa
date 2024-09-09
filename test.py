41d04459634741ecd695ceca0b919dcf052c0684
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2024-09-09 22:23:15 +0200

    Init

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
+class MovingAverage:
+    def __init__(self):
+        self.average = 0
+        self.count = 0
+        self.started = False
+
+    def add(self, num):
+        if self.started == False:
+            self.average = num
+            self.count = num
+            self.started = True
+        else:
+            self.average = (self.count / (self.count + 1) * self.average) + (num / (self.count + 1))
+            count = count + 1
+
+        return self.average
+
+    def get_average(self):
+        return self.average
+
+averages = [MovingAverage(), MovingAverage()]
+
 samples_file = open("./samples/95MHz-center-100MHzCW", "rb")
 
-started = False
-
-average = 0
-count = 0
+sample_index = 0
 
 while True:
     sample = samples_file.read(1)
@@ -13,12 +32,8 @@ while True:
 
     sample_int = int.from_bytes(sample)
 
-    if started == False:
-        started = True
-        average = sample_int
-        count = 1
-    else:
-        average = (count / (count + 1) * average) + (sample_int / (count + 1))
-        count = count + 1
+    print(sample_index % 2)
+
+    sample_index = sample_index + 1
 
 print(average)
